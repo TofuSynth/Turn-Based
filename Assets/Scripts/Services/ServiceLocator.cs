5e39a4404bdda21227f0227a93a6aec3252618a6
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-03-16 18:40:54 -0300

    Starting to build a service locator

--- Assets/Scripts/Services/ServiceLocator.cs
+++ Assets/Scripts/Services/ServiceLocator.cs
@@ -1,12 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class ServiceLocator : MonoBehaviour
+namespace Tofu.TurnBased.Services
 {
-    private void Awake()
+    public class ServiceLocator : MonoBehaviour
     {
+        private static ServiceLocator m_instance;
+        private Dictionary<Type, ServiceBase> m_services = new Dictionary<Type, ServiceBase>();
+        
+        void Awake()
+        {
+            m_instance = this;
+        }
+
+        public static void Register<TServiceType>(TServiceType service) where TServiceType : ServiceBase
+        {
+            Type type = typeof(TServiceType);
+            m_instance.m_services.Add(type, service); 
+        }
+
+        public static void Register(Type registrationType,ServiceBase service)
+        {
+            m_instance.m_services.Add(registrationType, service);
+        }
         
     }
 }
