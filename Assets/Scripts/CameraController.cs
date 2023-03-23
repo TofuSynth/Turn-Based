9be2e5b6f054cba064b72a1f75805ecb89050ca4
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-03-23 17:56:57 -0300

    Fix the layer obstruction check, add a cylinder to test that obstructions work, tidy up code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public GameObject cameraHandler;
     [SerializeField] private float m_cameraDistance;
-    [SerializeField] float rotateSpeed;
-    [SerializeField] private int damping;
+    [SerializeField] float m_rotateSpeed;
+    [SerializeField] private int m_damping;
     public LayerMask worldLayer;
 
     private void Start()
@@ -39,10 +40,10 @@ public class CameraController : MonoBehaviour
         Vector3 angles = cameraHandler.transform.rotation.eulerAngles;
 
         float horizontal = (angles.y + 180f) % 360f - 180f;
-        horizontal += Input.GetAxis("Mouse X") * rotateSpeed;
+        horizontal += Input.GetAxis("Mouse X") * m_rotateSpeed;
 
         float vertical = (angles.x + 180f) % 360f - 180f;
-        vertical -= Input.GetAxis("Mouse Y") * rotateSpeed;
+        vertical -= Input.GetAxis("Mouse Y") * m_rotateSpeed;
         vertical = Mathf.Clamp(vertical, -5f, 89f);
             
         cameraHandler.transform.rotation = Quaternion.Euler(new Vector3(vertical, horizontal, angles.z));
@@ -56,11 +57,11 @@ public class CameraController : MonoBehaviour
         Vector3 cameraAdjustment = transform.localPosition;
         if (Physics.Raycast(cameraObstruction, cameraToPlayer.magnitude - 1, worldLayer))
         {
-            cameraAdjustment.z += (damping * Time.deltaTime);
+            cameraAdjustment.z += (m_damping * Time.deltaTime);
         }
         else if (transform.localPosition.z > m_cameraDistance)
         {
-            cameraAdjustment.z -= (damping * Time.deltaTime);
+            cameraAdjustment.z -= (m_damping * Time.deltaTime);
         }
         transform.localPosition = cameraAdjustment;
     }
