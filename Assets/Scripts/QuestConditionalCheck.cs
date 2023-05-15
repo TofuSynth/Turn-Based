42335943bd6f1b25175107ffce6b3600d75ae33d
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-05-15 20:27:52 -0300

    Brainstorming Quest logic

---
+++ Assets/Scripts/QuestConditionalCheck.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestConditionalCheck : MonoBehaviour
+{
+    [SerializeField] private QuestToken m_quest;
+    public QuestToken Quest
+    {
+        get { return m_quest; }
+    }
+    [SerializeField] private int m_requiredStep;
+    public int RequiredStep
+    {
+        get { return m_requiredStep; }
+    }
+    
+    public void CheckQuestProgress()
+    {
+        if (Quest.QuestProgress == RequiredStep)
+        {
+            
+        }
+    }
+}
