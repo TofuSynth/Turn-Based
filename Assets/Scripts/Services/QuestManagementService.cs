f7be0a31347d56ee854426accd7c80d301098b25
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-06-21 14:55:31 -0300

    Create quest advancement and quest completion functions

--- Assets/Scripts/Services/QuestManagementService.cs
+++ Assets/Scripts/Services/QuestManagementService.cs
@@ -17,42 +17,102 @@ namespace Tofu.TurnBased.Quests
             if (!m_activeQuests.ContainsKey(newQuest))
             {
                 m_activeQuests.Add(newQuest, new QuestConditionals());
-                SetConditionals(newQuest);
+                UpdateConditionals(newQuest);
             }
         }
 
+        public void UpdateConditionals(QuestToken quest)
+        {
+            QuestConditionals newCondtionals = m_activeQuests[quest];
+            int currentStep = newCondtionals.currentStep;
+            if (currentStep > quest.Steps.Count)
+            {
+                CompleteQuest(quest);
+            }
+            else
+            {
+                QuestStep currentQuestStep = quest.Steps[currentStep];
+
+                foreach (QuestToken completedQuest in currentQuestStep.Conditions.QuestCompleted)
+                {
+                    newCondtionals.questCompleted.Add(quest, 1);
+                }
+
+                foreach (DialogueToken NPC in currentQuestStep.Conditions.SpeakTo)
+                {
+                    newCondtionals.spokenTo.Add(NPC, 1);
+                }
+
+                foreach (SceneToken area in currentQuestStep.Conditions.AreaVisited)
+                {
+                    newCondtionals.areaVisited.Add(area, 1);
+                }
+
+                foreach (EnemyToken enemies in currentQuestStep.Conditions.RequiredKills.Keys)
+                {
+                    newCondtionals.enemyDefeated.Add(enemies, currentQuestStep.Conditions.RequiredKills[enemies]);
+                }
+
+                foreach (UsableItemToken item in currentQuestStep.Conditions.ItemRequired.Keys)
+                {
+                    newCondtionals.itemGathered.Add(item, currentQuestStep.Conditions.ItemRequired[item]);
+                }
+            }
+        }
+
+        public void CheckIfQuestStepAdvances(QuestToken quest)
+        {
+            if (!CheckIfQuestConditionsMet(quest))
+            {
+                m_activeQuests[quest].currentStep++;
+                UpdateConditionals(quest);
+            }
+        }
+
+        public bool CheckIfQuestConditionsMet(QuestToken quest)
+        {
+            foreach (KeyValuePair<QuestToken, int> questCompleted in m_activeQuests[quest].questCompleted)
+            {
+                if (questCompleted.Value != 0)
+                {
+                    return false;
+                }   
+            }
+            foreach (KeyValuePair<DialogueToken, int> spokenTo in m_activeQuests[quest].spokenTo)
+            {
+                if (spokenTo.Value != 0)
+                {
+                    return false;
+                } 
+            }
+            foreach (KeyValuePair<SceneToken, int> areaVisited in m_activeQuests[quest].areaVisited)
+            {
+                if (areaVisited.Value != 0)
+                {
+                    return false;
+                } 
+            }
+            foreach (KeyValuePair<EnemyToken, int> enemyDefeated in m_activeQuests[quest].enemyDefeated)
+            {
+                if (enemyDefeated.Value != 0)
+                {
+                    return false;
+                } 
+            }
+            foreach (KeyValuePair<UsableItemToken, int> itemGathered in m_activeQuests[quest].itemGathered)
+            {
+                if (itemGathered.Value != 0)
+                {
+                    return false;
+                } 
+            }
+            return true;
+        }
+        
         public void CompleteQuest(QuestToken completedQuest)
         {
             m_activeQuests.Remove(completedQuest);
             m_completedQuests.Add(completedQuest);
-        }
-
-        public void SetConditionals(QuestToken newQuest)
-        {
-            QuestConditionals newCondtionals = m_activeQuests[newQuest];
-            int currentStep = newCondtionals.currentStep;
-            QuestStep currentQuestStep = newQuest.Steps[currentStep];
-            
-            foreach (QuestToken quest in currentQuestStep.Conditions.QuestCompleted)
-            { 
-                newCondtionals.questCompleted.Add(quest, 1);
-            }
-            foreach (DialogueToken NPC in currentQuestStep.Conditions.SpeakTo)
-            { 
-                newCondtionals.spokenTo.Add(NPC, 1);
-            }
-            foreach (SceneToken area in currentQuestStep.Conditions.AreaVisited)
-            { 
-                newCondtionals.areaVisited.Add(area, 1);
-            }
-            foreach (EnemyToken enemies in currentQuestStep.Conditions.RequiredKills.Keys)
-            { 
-                newCondtionals.enemyDefeated.Add(enemies, currentQuestStep.Conditions.RequiredKills[enemies]);
-            }
-            foreach (UsableItemToken item in currentQuestStep.Conditions.ItemRequired.Keys)
-            {
-                newCondtionals.itemGathered.Add(item, currentQuestStep.Conditions.ItemRequired[item]);
-            }
         }
     }
 
