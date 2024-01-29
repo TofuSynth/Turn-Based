3c871a012ebd7671378c40d25f90a448ef7d94a4
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2024-01-29 16:07:22 -0400

    Add Disclaimer page.

---
+++ Assets/Scripts/Utility/StartScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class StartCorrectScene
+{
+    static StartCorrectScene()
+    {
+        var startScene = EditorBuildSettings.scenes[0].path;
+        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(startScene);
+        EditorSceneManager.playModeStartScene = sceneAsset;
+    }
+}
